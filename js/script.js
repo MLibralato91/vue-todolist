a2979b990b7b173a60f44a04d278c8b3ac5098b0
MLibralato91 <michelelibralato911@gmail.com>
2023-03-30 18:39:59 +0200

    i take boostrap label for my project and i put my code inside

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 /*
-Creare una lista spesa Pasquetta
+Descrizione:
+Rifare l'esercizio della to do list.
+Ogni todo sarà un oggetto, formato da due proprietà:
+- text, una stringa che indica il testo del todo
+- done, un booleano (true/false) che indica se il todo è stato fatto oppure no
 
-campo di input in cui inserisco le varie cose da comprare
-Aggiungere due btn per ogni prodotto
+MILESTONE 1
+Stampare all'interno di una lista HTML un item per ogni todo.
+Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 
-uno per confermare l'elemento comprato
-ed uno per cancellare gli elementi dalla lista
+MILESTONE 2
+Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+
+MILESTONE 3
+Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+
+Bonus:
+1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
 
  */
 
@@ -26,29 +38,35 @@ createApp({
         },
       ],
 
-      newItem: [
-        {
-          name: '',
-          completed: false
-        }
-      ],
+      newIngredient: '',
+
     }
 
   },
   methods: {
     addItem() {
 
-      this.shoppingList.push(this.newItem)
-      this.newItem.name = ''
+      newItem = {
+        name: this.newIngredient,
+        completed: false
+      };
+      this.shoppingList.push(newItem);
+      this.newIngredient = '';
 
     },
     deleteItem(index) {
       this.shoppingList.splice(index, 1)
-    }
+    },
+    done(index) {
+      if (this.shoppingList[index].completed) {
+        this.shoppingList[index].completed = false;
+      } else {
+        this.shoppingList[index].completed = true;
+      }
+    },
 
 
   },
-
   mounted() {
 
   }
